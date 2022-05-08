960ed49cf7becb0e0fa65826dc90fe69260902ca
ksl29 <ksl29@njit.edu>
2022-05-08 05:28:22 -0400

    added more files and got project working

--- app/__init__.py
+++ app/__init__.py
@@ -9,25 +9,35 @@ from app.cli import create_database
 from app.db import db, database
 from app.db.models import User
 from app.error_handlers import error_handlers
+from app.logging_config import log_con, LOGGING_CONFIG
+from app.simple_pages import simple_pages
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
-    app.secret_key = 'This is an INSECURE secret!! DO NOT use this in production!!'
+    if os.environ.get("FLASK_ENV") == "production":
+        app.config.from_object("app.config.ProductionConfig")
+    elif os.environ.get("FLASK_ENV") == "development":
+        app.config.from_object("app.config.DevelopmentConfig")
+    elif os.environ.get("FLASK_ENV") == "testing":
+        app.config.from_object("app.config.TestingConfig")
+
     # added bootstrap 5 to project
     bootstrap = Bootstrap5(app);
     # these load functions with web interface
     app.register_blueprint(database)
     app.register_blueprint(auth)
+    app.register_blueprint(simple_pages)
     # these load functionality without a web interface
     app.register_blueprint(error_handlers)
-    db_dir = "../database/db.sqlite"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.abspath(db_dir)
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.init_app(app)
+    app.register_blueprint(log_con)
     # add command function to cli commands
     app.cli.add_command(create_database)
+    # add command function to cli commands
+    db.init_app(app)
+    app.cli.add_command(create_database)
+
 
     @app.route('/')
     def hello():
---
+++ app/simple_pages/__init__.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+simple_pages = Blueprint('simple_pages', __name__,
+                         template_folder='templates')
+
+
+@simple_pages.route('/')
+def index():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
