b7a3e0eba1f45d1108614f406f24710d1666a7f6
ksl29 <ksl29@njit.edu>
2022-05-12 04:53:11 -0400

    added READ pages

--- app/todo/forms/__init__.py
+++ app/todo/forms/__init__.py
@@ -5,9 +5,17 @@ from wtforms.fields import *
 
 class add_task_form(FlaskForm):
     title = TextAreaField('Title', [validators.DataRequired()], description="Please enter the task title")
-    completed = BooleanField()
+    description = TextAreaField('Description', description="Please enter a description")
+    due_date = DateTimeField('Due Date', [validators.DataRequired()], format='%Y-%m-%d')
+    is_important = BooleanField('Important?', [validators.DataRequired()])
+    is_completed = BooleanField('Completed?', [validators.DataRequired()])
     submit = SubmitField()
 
 
 class edit_task_form(FlaskForm):
+    title = TextAreaField('Title', [validators.DataRequired()], description="Please enter the task title")
+    description = TextAreaField('Description', description="Please enter a description")
+    due_date = DateTimeField('Due Date', [validators.DataRequired()], format='%Y-%m-%d')
+    is_important = BooleanField('Important?', [validators.DataRequired()])
+    is_completed = BooleanField('Completed?', [validators.DataRequired()])
     submit = SubmitField()
