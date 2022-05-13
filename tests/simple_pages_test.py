88163c4a80de9406aa463b615770a41d7aa4afdb
ksl29 <ksl29@njit.edu>
2022-05-13 00:12:41 -0400

    created task_test.py and updated other tests

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -6,30 +6,29 @@ def test_request_main_menu_links(client):
     assert response.status_code == 200
     assert b'href="/about"' in response.data
     assert b'href="/contact"' in response.data
-    #assert b'href="/tasks"' in response.data
-    assert b'href="/login"' in response.data
-    assert b'href="/register"' in response.data
+    assert b'href="/signin"' in response.data
+    assert b'href="/signup"' in response.data
 
 
 def test_request_index(client):
-    """This makes the index page"""
+    """tests if index page exists"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Index" in response.data
 
 def test_request_about(client):
-    """This makes the index page"""
+    """tests if about page exists"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About" in response.data
 
 def test_request_contact(client):
-    """This makes the index page"""
+    """tests if contact page exists"""
     response = client.get("/contact")
     assert response.status_code == 200
     assert b"Contact" in response.data
 
 def test_request_page_not_found(client):
-    """This makes the index page"""
+    """tests if page that doesn't exist returns a 404 error"""
     response = client.get("/freebitcoin")
     assert response.status_code == 404
